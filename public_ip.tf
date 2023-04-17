492d7b5adc25de93d2287682f313f375d5964ba9
terralien <terralienjp@gmail.com>
2023-04-17 16:42:09 +0900

    add public_ip

---
+++ public_ip.tf
@@ -0,0 +1,20 @@
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Dynamic" # "Static" または "Dynamic" を選択できます
+}
+
+//nicにpublic ipを追加
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
